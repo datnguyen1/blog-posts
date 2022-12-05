1f0296f1b8552cd8b277439ba2e14c6a28013e86
Dat Nguyen <64458422+datnguyen1@users.noreply.github.com>
2022-12-04 23:15:14 -0600

    Fixing Modal Window Duplicate Title Bug

--- static/script.js
+++ static/script.js
@@ -1,22 +1,24 @@
-function showModalWindow(title) {
+function showModalWindow(id) {
     // Get the modal element
-
     var modal = document.getElementById('modal-window');
     let h2 = document.getElementById('modal-title');
     let p = document.getElementById('modal-content');
 
-    // Retrieve the title and content values from the <div> element
-    let divElement = document.querySelector(`div[data-title="${title}"]`);
-    let content = divElement.getAttribute('data-content');
-    // Change the content of the h2 element
-    h2.innerHTML = title;
+    // Retrieve the <div> element using the id attribute
+    let divElement = document.getElementById(id);
 
-    // Change the content of the h2 element
+    // Retrieve the title and content values from the <div> element
+    let title = divElement.getAttribute('data-title');
+    let content = divElement.getAttribute('data-content');
+
+    // Change the content of the h2 and p elements
+    h2.innerHTML = title;
     p.innerHTML = content;
 
     // Set the modal's display property to "block" to show it
     modal.style.display = "block";
   }
+
   
   function unshowModalWindow() {
     // Get the modal element
